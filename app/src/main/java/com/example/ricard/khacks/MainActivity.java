38aa2c64568801d0362ed76709682ce75ba9ed3d
ricard2g <gascons1995@gmail.com>
2015-02-28 15:10:48 +0100

    Primeres coses

--- app/src/main/java/com/example/ricard/khacks/MainActivity.java
+++ app/src/main/java/com/example/ricard/khacks/MainActivity.java
@@ -5,8 +5,43 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    public void postData() {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://partners.api.skyscanner.net/apiservices/pricing/v1.0");
+
+        try {
+            // Add your data
+            List<NameValuePair>pairs = new ArrayList<NameValuePair>();
+            pairs.add(new BasicNameValuePair("key1", "value1"));
+            pairs.add(new BasicNameValuePair("key2", "value2"));
+            httppost.setEntity(new UrlEncodedFormEntity(pairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
